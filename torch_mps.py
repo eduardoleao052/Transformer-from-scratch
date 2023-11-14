5518c860606baca225f1e670a01f3a883faa6e72
Eduardo Leao <eduardoleao@Eduardos-MacBook-Air.local>
2023-11-14 10:04:53 -0300

    changes in local, not deprecated

--- torch_mps.py
+++
@@ -1,7 +0,0 @@
-﻿import torch
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    x = torch.ones(1, device=mps_device)
-    print (x*2)
-else:
-    print ("MPS device not found.")
