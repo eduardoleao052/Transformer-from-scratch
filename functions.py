294f0fa9f812855badfb01722617267361dae760
Eduardo Leao <eduardoleao@Eduardos-MacBook-Air.local>
2023-11-14 00:49:37 -0300

    added optimizers.py

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import numpy as np
-import pandas as pd
-import scipy.signal
+import logging
+import logging.handlers
 
 def add_padding(x,k):
     if k == 0:
@@ -45,11 +45,85 @@ def sigmoid(z, training = False):
         a = 1/(1+np.exp(-z))
         return a
 
+def clean_vocab(x, word):
+    if '\n' in word:
+        tokens_to_add = []
+        words_to_add = word.split('\n')
+        for i in words_to_add:
+            if i != '':
+               tokens_to_add.append(i)
+        clean_vocab(x,tokens_to_add[0])
+        for i in range(1,len(words_to_add)):
+            #x.append('\n')
+            clean_vocab(x,words_to_add[i])   
+        return
+    
+    if word.endswith(','):
+            clean_vocab(x, word[:-1])
+            x.append(',')
+    elif word.endswith('.'):
+            clean_vocab(x,word[:-1])
+            x.append('.')
+    elif word.endswith(':'):
+            clean_vocab(x,word[:-1])
+            x.append(':')
+    elif word.endswith('”'):
+            clean_vocab(x,word[:-1])
+            x.append('"')
+    elif word.endswith(')'):
+            clean_vocab(x,word[:-1])
+            x.append(')')
+    elif word.endswith('?'):
+            clean_vocab(x,word[:-1])
+            x.append('?')
+    elif word.endswith('!'):
+            clean_vocab(x,word[:-1])
+            x.append('!')
+    elif word.endswith(';'):
+            clean_vocab(x,word[:-1])
+            x.append(';')
+    elif word.endswith('...'):
+            clean_vocab(x,word[:-3])
+            x.append('...')
+    elif word.endswith("…"):
+            clean_vocab(x,word[:-1])
+            x.append('...')
+    elif word.startswith('“'):
+            x.append('"')
+            clean_vocab(x,word[1:])
+    elif word.startswith('('):
+            x.append('(')
+            clean_vocab(x,word[1:])
+    elif word.startswith('…'):
+            x.append('...')
+            clean_vocab(x,word[1:])
 
-#print(cross_correlate(x,y))
-#print(scipy.signal.correlate2d(x,y,mode = 'valid'))
-#x = add_padding(x,0)
-#print(x)
-#print(convolute(x,y))
-#print(scipy.signal.convolve2d(x,y,mode = 'valid'))
+    if (not word.endswith(',')) and (not word.endswith('.')) and (not word.endswith(':')) and (not word.endswith(';')) and (not word.endswith('…'))  and (not word.endswith('!')) and (not word.endswith('?')) and (not word.endswith(')')) and (not word.endswith('...')) and (not word.endswith('”')) and (not word.startswith('“')) and (not word.startswith('(')) and (not word.startswith('…')):
+        if word != '':
+            x.append(word)
+            
+def build_logger(sender, pwd):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
+
+    file_handler = logging.FileHandler("/Users/eduardoleao/Documents/ML/NN/rnn/training.log")
+    smtpHandler = logging.handlers.SMTPHandler(
+    mailhost=("smtp.gmail.com",587),
+    fromaddr=sender,
+    toaddrs=sender,
+    subject="Training Alert",
+    credentials=(sender, pwd),
+    secure=()
+    )
+
+    file_handler.setLevel(logging.INFO)
+    smtpHandler.setLevel(logging.WARNING)
+
+    file_handler.setFormatter(formatter)
+    smtpHandler.setFormatter(formatter)
+
+    logger.addHandler(smtpHandler)
+    logger.addHandler(file_handler)
+    return logger
--- optimizers.py
+++ optimizers.py
@@ -1,35 +1,5 @@
 import numpy as np
-
-def Adam(Wxh, Whh, Wha, bh, ba, dWxh, dWhh, dWha, dbh, dba, config):
-    #UPDATE Weights:
-    config['m_Wxh'] = (config['m_Wxh']*config['beta1'] + (1 - config['beta1']) * dWxh) / (1- config['beta1']**config['t'])
-    config['v_Wxh'] = (config['v_Wxh']*config['beta2'] + (1 - config['beta2']) * np.square(dWxh)) / (1- config['beta2']**config['t'])
-
-    next_Wxh = Wxh - (config["learning_rate"] * config['m_Wxh']) / (np.sqrt(config['v_Wxh']) + config['epsilon']) - config["regularization"] * config["learning_rate"] * Wxh
-   
-    config['m_Whh'] = (config['m_Whh']*config['beta1'] + (1 - config['beta1']) * dWhh) / (1- config['beta1']**config['t'])
-    config['v_Whh'] = (config['v_Whh']*config['beta2'] + (1 - config['beta2']) * np.square(dWhh)) / (1- config['beta2']**config['t'])
-    
-    next_Whh = Whh - (config["learning_rate"] * config['m_Whh']) / (np.sqrt(config['v_Whh']) + config['epsilon'])- config["regularization"] * config["learning_rate"] * Whh
-
-    config['m_Wha'] = (config['m_Wha']*config['beta1'] + (1 - config['beta1']) * dWha) / (1- config['beta1']**config['t'])
-    config['v_Wha'] = (config['v_Wha']*config['beta2'] + (1 - config['beta2']) * np.square(dWha)) / (1- config['beta2']**config['t'])
-    
-    next_Wha = Wha - (config["learning_rate"] * config['m_Wha']) / (np.sqrt(config['v_Wha']) + config['epsilon'])- config["regularization"] * config["learning_rate"] * Wha
-
-    config['m_bh'] = (config['m_bh']*config['beta1'] + (1 - config['beta1']) * dbh) / (1- config['beta1']**config['t'])
-    config['v_bh'] = (config['v_bh']*config['beta2'] + (1 - config['beta2']) * np.square(dbh)) / (1- config['beta2']**config['t'])
-    
-    next_bh = bh - (config["learning_rate"] * config['m_bh']) / (np.sqrt(config['v_bh']) + config['epsilon'])
-
-    config['m_ba'] = (config['m_ba']*config['beta1'] + (1 - config['beta1']) * dba) / (1- config['beta1']**config['t'])
-    config['v_ba'] = (config['v_ba']*config['beta2'] + (1 - config['beta2']) * np.square(dba)) / (1- config['beta2']**config['t'])
-    
-    next_ba = ba - (config["learning_rate"] * config['m_ba']) / (np.sqrt(config['v_ba']) + config['epsilon'])
-
-    config['t'] += 1
-
-    return next_Wxh, next_Whh, next_Wha, next_bh, next_ba, config
+import torch
 
 def SGD(b,w,db,dw,config):
     next_w = w - config['learning_rate'] * dw - config['learning_rate'] * config['regularization'] * w
@@ -54,20 +24,54 @@ def Momentum(Wxh, Whh, Wha, bh, ba, dWxh, dWhh, dWha, dbh, dba, config):
 
     return next_Wxh, next_Whh, next_Wha, next_bh, next_ba, config
 
+def Adam(parameters, gradients, config):
 
-def Adam_LSTM(parameters, gradients, config):
     for i in parameters.keys():
-        if i.startswith('W'):
-            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) / (1- config['beta1']**config['t'])
-            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * np.square(gradients['d{}'.format(i)])) / (1- config['beta2']**config['t'])
+        if i.startswith('W') or i.startswith('E'):
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) #/ (1- config['beta1']**config['t'])
+            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * np.square(gradients['d{}'.format(i)])) #/ (1- config['beta2']**config['t'])
+
+            parameters[i] = parameters[i] - (config['learning_rate'] * config["m_{}".format(i)]) / (np.sqrt(config["v_{}".format(i)]) + config['epsilon']) - config['regularization'] * config['learning_rate'] * (parameters[i]**2)
         
-            parameters[i] = parameters[i] - (config["learning_rate"] * config["m_{}".format(i)]) / (np.sqrt(config["v_{}".format(i)]) + config['epsilon']) - config['regularization'] * config['learning_rate'] * parameters[i]
+        elif i.startswith('b') or i.startswith('gamma'):
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) #/ (1- config['beta1']**config['t'])
+            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * np.square(gradients['d{}'.format(i)])) #/ (1- config['beta2']**config['t'])
+
+            parameters[i] = parameters[i] - (config['learning_rate'] * config["m_{}".format(i)]) / (np.sqrt(config["v_{}".format(i)]) + config['epsilon'])
+
+    config['t'] += 1
+
+    return parameters, config
+
+def SGD_Momentum(parameters, gradients, config):
+
+    for i in parameters.keys():
+        if i.startswith('W') or i.startswith('E'):
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)])
+        
+            parameters[i] = parameters[i] - (config["learning_rate"] * config["m_{}".format(i)]) - config['regularization'] * config['learning_rate'] * (parameters[i]**2)
         
         elif i.startswith('b'):
-            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) / (1- config['beta1']**config['t'])
-            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * np.square(gradients['d{}'.format(i)])) / (1- config['beta2']**config['t'])
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)])
         
-            parameters[i] = parameters[i] - (config["learning_rate"] * config["m_{}".format(i)]) / (np.sqrt(config["v_{}".format(i)]) + config['epsilon'])
+            parameters[i] = parameters[i] - (config["learning_rate"] * config["m_{}".format(i)])
+
+    return parameters, config
+
+def TorchAdam(parameters, gradients, config):
+
+    for i in parameters.keys():
+        if i.startswith('W') or i.startswith('E'):
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) #/ (1- config['beta1']**config['t'])
+            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * torch.square(gradients['d{}'.format(i)])) #/ (1- config['beta2']**config['t'])
+
+            parameters[i] = parameters[i] - (config['learning_rate'] * config["m_{}".format(i)]) / (torch.sqrt(config["v_{}".format(i)]) + config['epsilon']) - config['regularization'] * config['learning_rate'] * (parameters[i]**2)
+        
+        elif i.startswith('b') or i.startswith('gamma'):
+            config["m_{}".format(i)] = (config["m_{}".format(i)]*config['beta1'] + (1 - config['beta1']) * gradients['d{}'.format(i)]) #/ (1- config['beta1']**config['t'])
+            config["v_{}".format(i)] = (config["v_{}".format(i)]*config['beta2'] + (1 - config['beta2']) * torch.square(gradients['d{}'.format(i)])) #/ (1- config['beta2']**config['t'])
+
+            parameters[i] = parameters[i] - (config['learning_rate'] * config["m_{}".format(i)]) / (torch.sqrt(config["v_{}".format(i)]) + config['epsilon'])
 
     config['t'] += 1
 
