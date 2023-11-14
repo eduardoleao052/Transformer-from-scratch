3a8bd8d26c87d18eb6a05c524d02536420be0cf9
Eduardo Leao <eduardoleao@Eduardos-MacBook-Air.local>
2023-11-14 00:43:59 -0300

    added run bash script and requirements.txt

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Initializing training of NLP model..."
+python3 run.py --train /Users/eduardoleao/Documents/ML/NN/rnn/config.json /Users/eduardoleao/Documents/ML/NN/rnn/data/sanderson.txt /Users/eduardoleao/Documents/ML/NN/rnn/models/model_01.json
+
+
